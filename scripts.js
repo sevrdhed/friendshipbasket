15543749fc742f0972fe5030863e0d9effa3b4bb
Steve Heath <sheath6633@yahoo.com>
2019-11-25 16:29:33 -0700

    All functions working, need work on the UI and need to greatly expand the options of ingredients across all subcategories, particularly in the Dessert section

--- scripts.js
+++ scripts.js
@@ -1,36 +1,83 @@
-var proteins = ["Carne Asada", "Pre-cooked chicken", "Hard Boiled Egg", "Lamb", "Game Hen", "Pork Chop", "Baked Ham", "Bacon", "Taco Meat", "Smoked Salmon", "Beef Jerky", "Tofu (you're welcome sarah)", "Ground bison", "Pre-cooked chicken wings", "Hot Italian sausage", "Tilapia", "Stew Meat", "Greek Yogurt"]
+//Need to remove Beverages and re-do sugars into "desserts"
+//Perhaps have multiple categories specific to dessert, that broaden the potential result set
+
+var proteins = ["Carne Asada", "Rotisserie chicken", "Hard Boiled Egg", "Lamb", "Game Hen", "Pork Chop", "Baked Ham", "Bacon", "Taco Meat", "Smoked Salmon", "Beef Jerky", "Tofu (you're welcome sarah)", "Ground bison", "Pre-cooked chicken wings", "Hot Italian sausage", "Tilapia", "Stew Meat", "Greek Yogurt"]
 var vegetable = ["Arugula", "Cucumber", "Cauliflower", "Broccoli", "Snap Peas", "Roasted Red Peppers", "Mukimane", "Canned Artichokes", "Lemon Grass", "White Mushrooms", "Turnips", "Rainbow Carrots", "Chard", "Canned Corn", "Shallots", "Garlic", "Sun Dried Tomatos"]
 var fruit = ["Strawberries", "Mandarin Orange Cups", "Pineapple Chunks", "Dragon Fruit", "Raspberries", "Grapes", "Pear", "Apple", "Black Plums", "Mangoes", "Kiwi", "Cherry Pie Filling", "Fruit Cocktail", "Grapefruit"]
 var weird = ["Jalapeno Chips", "Cheetos", "Flamin Hot Cheetos", "Cinnamon", "Crushed Red Pepper", "Spicy Pickles"]
 var beverages = ["Whiskey", "Rum", "Whipped Cream Vodka", "The Kraken", "Champagne", "Malt Liquor", "Coke"]
-var fats = ["Duck Fat", "Agave Nectar", "Turbino", "Lard", "Irish Butter", "Bacon Fat"]
-var sugars = ["Twinkies", "Little Debbie Oatmeal Pies", "Donuts", "Chocolate Bar", "Candy Cane", "Sour Patch Kids", "Marshmellows", "Lemon Hard Candy", "Gum Drops", "Peeps"]
+var fats = ["Duck Fat", "Agave Nectar", "Lard", "Irish Butter", "Bacon Fat"]
+var sugars = ["Twinkies", "Raw Sugar", "Little Debbie Oatmeal Pies", "Donuts", "Chocolate Bar", "Candy Cane", "Sour Patch Kids", "Marshmellows", "Lemon Hard Candy", "Gum Drops", "Peeps"]
 var carbstarch = ["Baked Potato", "Hawaiian Rolls", "Crostinis", "Minute Rice", "Brown Rice", "Stuffing Shells", "Mac and Cheese", "Cavatappi Pasta", "Saltine Crackers", "Hamburger Buns", "Taco Shells", "Frozen Lasagne", "Wonton Wrappers"]
 
-
-
 function returnEntree(){
-  var rand = proteins[Math.floor(Math.random() * proteins.length)];
-  document.getElementById('entreeDisplay').innerHTML = rand;
-  console.log(rand)
+
+  //Create the array of available sub categories and empty array of used sub categories
+  //var potentialCat = ["proteins", "vegetable", "fruit", "weird", "beverages", "fats", "carbstarch"];
+  var potentialCat = [proteins, vegetable, fruit, weird, fats, carbstarch];
+  var usedCat = [];
+  var selectedItems = [];
+  
+  //Iterate through 4 times, randomly picking categories from the list of potential categories and removing them from that list
+  for(i = 0; i<4; i++){
+    var rand = [Math.floor(Math.random() * potentialCat.length)]
+    usedCat.push(potentialCat[rand]);
+    potentialCat.splice(rand, 1);
+    //console.log(usedCat[i])
+  }
+  //Use the used categories list an grab an individual item from each of those lists.
+  for(i = 0; i<4; i++){
+    var rand = [Math.floor(Math.random() * usedCat[i].length)]
+    selectedItems.push(usedCat[i][rand])
+    console.log(i, selectedItems[i])
+  }
+  document.getElementById('entreeDisplay').innerHTML = "Ingredients are: " + selectedItems[0] + ", " + selectedItems[1] + ", " + selectedItems[2] + ", " + selectedItems[3] + "<br><br>"
 }
 
+function returnAppetizer(){
 
-function returnEntreeTwo(){
-
-  //This randomizes and then returns an array of sub-categories
-  var potentialCat = ["proteins", "vegetable", "fruit", "weird", "beverages"];
+  //Create the array of available sub categories and empty array of used sub categories
+  var potentialCat = [proteins, vegetable, fruit, weird, fats, carbstarch];
   var usedCat = [];
+  var selectedItems = [];
+  
+  //Iterate through 4 times, randomly picking categories from the list of potential categories and removing them from that list
   for(i = 0; i<4; i++){
-  var rand = [Math.floor(Math.random() * potentialCat.length)]
-  console.log(rand);
-  usedCat.push(potentialCat[rand]);
-  potentialCat.splice(rand, 1);
-  console.log(usedCat[i])
+    var rand = [Math.floor(Math.random() * potentialCat.length)]
+    usedCat.push(potentialCat[rand]);
+    potentialCat.splice(rand, 1);
+ 
+  }
+ 
+  for(i = 0; i<4; i++){
+    var rand = [Math.floor(Math.random() * usedCat[i].length)]
+    selectedItems.push(usedCat[i][rand])
+    console.log(i, selectedItems[i])
+  }
+  document.getElementById('appetizerDisplay').innerHTML = "Ingredients are: " + selectedItems[0] + ", " + selectedItems[1] + ", " + selectedItems[2] + ", " + selectedItems[3] + "<br><br>"
+}
+
+//This needs to be refactored to provide better dessert type ingredients, as the list of ingredients from each sub category do not currently create good baskets
+function returnDessert(){
+
+  var potentialCat = [fruit, weird, sugars, sugars];
+  var usedCat = [];
+  var selectedItems = [];
+  
+  
+  for(i = 0; i<4; i++){
+    var rand = [Math.floor(Math.random() * potentialCat.length)]
+    usedCat.push(potentialCat[rand]);
+    potentialCat.splice(rand, 1);
   
   }
-  document.getElementById('entreeDisplay').innerHTML = "Sub Categories are: " + usedCat[0] +" "+ usedCat[1] + " " + usedCat[2] + " " + usedCat[3];
-  //document.getElementById('entreeDisplay').innerHTML = 'Steve';
+  
+  for(i = 0; i<4; i++){
+    var rand = [Math.floor(Math.random() * usedCat[i].length)]
+    selectedItems.push(usedCat[i][rand])
+    console.log(i, selectedItems[i])
+  }
+  document.getElementById('dessertDisplay').innerHTML = "Ingredients are: " + selectedItems[0] + ", " + selectedItems[1] + ", " + selectedItems[2] + ", " + selectedItems[3] + "<br><br>"
 }
 //Current plan. Create an array for each category type that contains the possible sub-category options for that category
 //Randomize a selection from that array, put that into a new array, then remove it for that run of the generator (prevent duplicates)
